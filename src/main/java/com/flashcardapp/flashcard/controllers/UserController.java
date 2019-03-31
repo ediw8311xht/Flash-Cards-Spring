79f1a165157ea66ed6616d138db359edacb250fd
Max <maxballard333@gmail.com>
2019-03-31 19:20:04 -0400

    S

--- src/main/java/com/flashcardapp/flashcard/controllers/UserController.java
+++ src/main/java/com/flashcardapp/flashcard/controllers/UserController.java
@@ -1,16 +1,16 @@
 package com.flashcardapp.flashcard.controllers;
 
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import com.flashcardapp.flashcard.ent.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.flashcardapp.flashcard.repositories.UserRepository;
 
 @Controller
@@ -18,11 +18,19 @@ import com.flashcardapp.flashcard.repositories.UserRepository;
 public class UserController {
 
     private UserRepository userRepo;
-    private PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository userRepo) {
         this.userRepo = userRepo;
-        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/me")
+    public String MyProfile(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user_n = userRepo.findOne(auth.getName());
+        model.addAttribute("username", user_n.getUsername());
+        model.addAttribute("flashsets", user_n.getFlashsetsStr());
+        return "User";
+
     }
 
     @GetMapping("/")
