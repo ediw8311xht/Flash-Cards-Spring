2d8c9d70ce3d3d3e42ac4ca8dea88c2b4b8ac27b
maceurt <maxballard333@gmail.com>
2019-05-01 15:10:57 -0400

    Update MySets.js

--- src/main/resources/static/js/MySets.js
+++ src/main/resources/static/js/MySets.js
@@ -4,15 +4,17 @@
 $(document).ready(function(){
 
     $(".delete-flashset").on("click", function(event) {
-        let del_par_el = $(this).parent();
-        $.post("/flashcard/ajax/Flashcardset/delete", {"id": del_par_el.find(".flashset-id").text()},
-                function(data) {
-                    del_par_el.remove();
-                    if (data == "Success") {
+        if (del_bool) {
+            let del_par_el = $(this).parent();
+            $.post("/flashcard/ajax/Flashcardset/delete", {"id": del_par_el.find(".flashset-id").text()},
+                    function(data) {
                         del_par_el.remove();
-                    }
-                 }, "text");
+                        if (data == "Success") {
+                            del_par_el.remove();
+                        }
+                     }, "text");
 
-    });
+        });
+    }
 
 });
