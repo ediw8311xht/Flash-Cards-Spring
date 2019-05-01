61a1a4b545cab3f56b89f816117d30311860d67b
maceurt <maxballard333@gmail.com>
2019-05-01 15:19:59 -0400

    Update MySets.js

--- src/main/resources/static/js/MySets.js
+++ src/main/resources/static/js/MySets.js
@@ -4,6 +4,7 @@
 $(document).ready(function(){
 
     $(".delete-flashset").on("click", function(event) {
+        let del_bool = confirm("Do you really want to delete that flashset?");
         if (del_bool) {
             let del_par_el = $(this).parent();
             $.post("/flashcard/ajax/Flashcardset/delete", {"id": del_par_el.find(".flashset-id").text()},
